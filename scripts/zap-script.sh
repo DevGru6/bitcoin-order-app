8218d7bd951d20925e58f1d40d63d4f874e7ca16
test <test>
2024-07-16 16:14:23 +0800

    lint, dast,sast

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://devgru6.github.io/bitcoin-order-app/" -order-app/" -l PASS > zap_baseline_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://devgru6.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 echo $? > /dev/null
